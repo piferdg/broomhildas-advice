ba247ab3f494cc9e4fc9902638e96f9a2fbe89c4
Danny Pifer <piferdg@gmail.com>
2018-07-19 12:05:17 -0600

    Initial commit for advice page

---
+++ app.js
@@ -0,0 +1,23 @@
+const apiURL = ('http://api.adviceslip.com/advice')
+
+button = document.getElementsByTagName('button')[0]
+button.addEventListener('click', advice)
+
+let info = document.getElementsByTagName('h2')[0]
+let text = document.createElement('p')
+
+function advice(event) {
+  fetch (apiURL)
+  .then (response => response.json())
+  .then (myJSON = myJSON => {
+      newAdvice(myJSON)
+  })
+}
+
+function newAdvice(myJSON) {
+  let adviceText = myJSON.slip.advice
+
+  text.textContent = adviceText
+  info.appendChild(text)
+}
+
